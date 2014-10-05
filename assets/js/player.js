ad910e1c9854276e58d1c9f8423a2278b6427415
paolomoretti <littlebrown@gmail.com>
2014-10-05 17:59:25 +0100

    Update to video player page

---
+++ assets/js/player.js
@@ -0,0 +1,48 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+
+  $(document).ready(function () {
+    player = new YT.Player("yt_player", {
+      height: "100%",
+      width: "100%",
+      videoId: "BM_dJaohniA",
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange
+      }
+    })
+  })
+}
+
+function onPlayerReady (event) {
+  startVideo = function () {
+    player.playVideo();
+    player.setVolume(100);
+  }
+}
+
+function onPlayerStateChange (event) {
+  console.log ("onPlayerStateChange", event);
+}
+
+
+function startVideo () {
+  setTimeout(function () {
+    startVideo();
+  }, 300);
+}
+
+setTimeout(function () {
+  $(".slide-1").addClass("show");
+
+  setTimeout(function () {
+    $(".slide-1").removeClass("show");
+    $(".video-player").css("opacity", 1);
+
+    setTimeout(function () {
+      $(".container").remove();
+    }, 2000);
+
+  }, 3000);
+}, 500);
